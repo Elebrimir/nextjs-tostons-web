b661873a99f8e8897a082b64d1ceeaa63f2c1b54
Pablo Cortés <cortesoltra@gmail.com>
2024-02-20 13:06:34 +0100

    REDIRECCIONAR A LARAVEL

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
         </a>
 
         <a
-          href='/login'
+          href='http://localhost:8000/login'
           className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
           rel='noopener noreferrer'
         >
